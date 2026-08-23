4530abe472d27b72b2c43d4bd38fdcab0d531867
Ryuta <lingmulongtai200@gmail.com>
2026-08-23 14:56:47 +0900

    chore(release): prepare v0.8.1 hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.codexbar.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.8.0"
+        versionCode = 24
+        versionName = "0.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
